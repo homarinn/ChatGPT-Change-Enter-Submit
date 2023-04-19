26849e0b61bfcb04d5640a6cd2b330452cd6fdcf
homare-asakura <homarinn1201@gmail.com>
2023-04-20 01:29:30 +0900

    タブがなぜかやけに多かったので削除

--- js/content_script.js
+++ js/content_script.js
@@ -3,22 +3,22 @@
 
 	const userAgent = window.navigator.userAgent.toLowerCase();
 	document.addEventListener('keydown', (e) => {
-			if (e.target.tagName !== 'TEXTAREA') {
-					return;
-			}
+		if (e.target.tagName !== 'TEXTAREA') {
+			return;
+		}
 
-			let withKey;
+		let withKey;
 
-			if (userAgent.indexOf("windows nt") !== -1) {
-					withKey = e.ctrlKey;
-			}
+		if (userAgent.indexOf("windows nt") !== -1) {
+			withKey = e.ctrlKey;
+		}
 
-			if (userAgent.indexOf("mac os x") !== -1) {
-					withKey = e.metaKey;
-			}
+		if (userAgent.indexOf("mac os x") !== -1) {
+			withKey = e.metaKey;
+		}
 
-			if (e.code == "Enter" && !withKey) {
-					e.stopPropagation();
-			}
+		if (e.code == "Enter" && !withKey) {
+			e.stopPropagation();
+		}
 	}, { capture: true });
 })();
